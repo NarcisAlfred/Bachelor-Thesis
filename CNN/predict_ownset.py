e11f1ee27d62bbb3129de5f9606ead3f92d9b0a1
Alfredovici <szenenarcis@gmail.com>
2023-06-14 15:36:25 +0300

    First push
    Pushed first layout of the CNN

---
+++ CNN/predict_ownset.py
@@ -0,0 +1,103 @@
+# USAGE
+# python predict.py
+# import the necessary packages
+from pyimagesearch import config
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import cv2
+import os
+import tensorflow as tf
+import sys
+from PIL import Image
+
+def prepare_plot(origImage, origMask, predMask):
+	# initialize our figure
+	figure, ax = plt.subplots(nrows=1, ncols=3, figsize=(10, 10))
+	# plot the original image, its mask, and the predicted mask
+	ax[0].imshow(origImage)
+	ax[1].imshow(origMask)
+	ax[2].imshow(predMask)
+	# set the titles of the subplots
+	ax[0].set_title("Image")
+	ax[1].set_title("Original Mask")
+	ax[2].set_title("Predicted Mask")
+	# set the layout of the figure and display it
+	figure.tight_layout()
+	figure.show()
+
+def make_predictions(model, imagePath):
+	# set model to evaluation mode
+	model.eval()
+	# turn off gradient tracking
+	with torch.no_grad():
+		# load the image from disk, swap its color channels, cast it to float data type, and scale its pixel values
+		image = cv2.imread(imagePath.path)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		image = image.astype("float32") / 255.0
+
+		# resize the image and make a copy of it for visualization
+		image = cv2.resize(image,(112,112))
+		orig = image.copy()
+
+		# find the filename and generate the path to ground truth mask
+		groundTruthPath = imagePaths[:-3] + r"Labeled" + "\%s"%path.name[:-4] + "_gt.jpg"
+
+		# load the ground-truth segmentation mask in grayscale mode and resize it
+		gtMask = cv2.imread(groundTruthPath, 0)
+		gtMask = cv2.resize(gtMask, (config.INPUT_IMAGE_HEIGHT,config.INPUT_IMAGE_HEIGHT))
+
+		# make the channel axis to be the leading one, add a batch dimension, create a PyTorch tensor, and flash it to the current device
+		image = np.transpose(image, (2, 0, 1))
+		image = np.expand_dims(image, 0)
+		image = torch.from_numpy(image).to(config.DEVICE)
+
+		# make the prediction, pass the results through the sigmoid function, and convert the result to a NumPy array
+		predMask = model(image).squeeze()
+		predMask = torch.sigmoid(predMask)
+		predMask = predMask.cpu().numpy()
+
+		# Convert tensors to numpy arrays
+		imPred = torch.tensor(predMask)
+		imLab = torch.tensor(gtMask)
+
+		accuracy = 100 * np.array(torch.sum(imPred == imLab))/12544
+
+		# filter out the weak predictions and convert them to integers
+		predMask = (predMask > config.THRESHOLD) * 255
+		predMask = predMask.astype(np.uint8)
+		
+		# check if the program is running in debug mode ( for plotting purposes)
+		gettrace = getattr(sys, 'gettrace', None)
+
+		# prepare a plot for visualization
+		if gettrace():
+			prepare_plot(orig, gtMask, predMask)
+
+		return accuracy
+
+
+
+# load the image path
+print("[INFO] Loading up test image paths...")
+imagePaths = r"C:\Users\Alfred\Desktop\Importante\Licenta\Dataset\Test\Raw"
+
+# load our model from disk and flash it to the current device
+print("[INFO] Loading up model...")
+unet = torch.load(config.MODEL_PATH).to(config.DEVICE)
+
+# Initialize the mean accuracy and the number of iterations
+count = 0
+mean_acc = 0
+# iterate over the randomly selected test image paths
+for path in os.scandir(imagePaths):
+	# make predictions and visualize the results
+	acc = make_predictions(unet, path)
+	mean_acc += acc
+	count += 1
+	# print the accuracy
+	print("Accuracy for {} is: {:.4f}".format(path.name,acc))
+
+# print the mean accuracy
+print("The mean accuracy of the dataset is: {:.4f}".format(mean_acc/count))
+
---
+++ CNN/pyimagesearch/config.py
@@ -0,0 +1,43 @@
+# import the necessary packages
+import torch
+import os
+
+# base path of the dataset
+DATASET_PATH = os.path.join("dataset", "train")
+
+# define the path to the images and masks dataset
+IMAGE_DATASET_PATH = os.path.join(DATASET_PATH, "Raw")
+MASK_DATASET_PATH = os.path.join(DATASET_PATH, "Labeled")
+
+# define the test split
+TEST_SPLIT = 0.15
+
+# determine the device to be used for training and evaluation
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# determine if we will be pinning memory during data loading
+PIN_MEMORY = True if DEVICE == "cuda" else False
+
+# and number of levels in the U-Net model
+NUM_CHANNELS = 1
+NUM_CLASSES = 1
+NUM_LEVELS = 3
+
+# initialize learning rate, number of epochs to train for, and the batch size
+INIT_LR = 0.001 # era 0.0005 inainte
+NUM_EPOCHS = 40 # era 50 inainte
+BATCH_SIZE = 64 # era 1 inainte
+
+# define the input image dimensions
+INPUT_IMAGE_WIDTH = 112
+INPUT_IMAGE_HEIGHT = 112
+
+# define threshold to filter weak predictions
+THRESHOLD = 0.5
+
+# define the path to the base output directory
+BASE_OUTPUT = "output"
+
+# define the path to the output serialized model, model training, plot and testing image paths
+MODEL_PATH = os.path.join(BASE_OUTPUT, "unet_tgs_salt.pth")
+PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot.png"])
